ea166507c02ef2c6d77ba853f2071a96434f3ebc
Kevin Haas <kevin.haas96@gmail.com>
2022-03-19 10:43:22 -0400

    added glossary db and updated page view

---
+++ public/script.js
@@ -0,0 +1,65 @@
+(function() {
+	'use strict';
+	
+	$('#glossary-entries').html(db.definitions.map(item => {
+		return $('<tr>').append([
+			$('<td>').text(item.term),
+			$('<td>').text(item.definition),
+			$('<td>').append(() => {
+				return (!item.sources.length) ? 
+					$('<span>').text('No Publications') : 
+					$('<details>').html([
+						$('<summary>').text('Publications'),
+						$('<ul>').append(item.sources.map(source => {
+							return $('<li>').append([
+								$('<a>').attr('target', '_blank').attr('href', source.url).text(source.name)
+							]);
+						}))
+					]);
+			})
+		])
+	}));
+	
+	$('input.filter').on('keyup', function() {
+		$(this).attr('value', this.value.trim() || null);
+	});
+
+	$('input.filter').on('keyup', function() {
+		let filters = $('input.filter[value]');
+		let rows = $('#glossary-entries>tr');
+		
+		if(!!filters.length) {
+			rows.each(function() {
+				let tr = $(this);
+				let tds = tr.find('td').attr('match', null);
+				
+				tds.each(function() {
+					let td = $(this);
+					
+					let value = filters.eq(td.index()).val();
+					let text = td.text().trim().split(/\n{1,}/g).join(' ');
+					
+					let state = null;
+					if(!!value) {
+						let match = new RegExp(value, 'gi');
+						if(match.test(text)) {
+							state = true;
+						}
+					}
+					
+					td.attr('match', state);
+				});
+				
+				if(!tds.filter('[match]').length) {
+					tr.hide();
+				}
+			});
+			
+			rows.has('td[match]').show();
+		} else {
+			rows.show().find('td').attr('match', true);
+		}
+	});
+	
+	$('input.filter').trigger('keyup');
+})();
